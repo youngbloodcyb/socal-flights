4563749bc8d3a9435f40e046d6fa3db02d9b1e48
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-21 20:07:41 -0700

    update styles for input component, add content block component

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,11 +1,20 @@
-// <!--
-//   This component uses @tailwindcss/forms
+export default function Input() {
+    return (
+        <label
+            className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+            >
+            <span className="text-xs font-medium text-gray-700"> Email </span>
 
-//   yarn add @tailwindcss/forms
-//   npm install @tailwindcss/forms
+        <input
+            type="email"
+            id="UserEmail"
+            placeholder="JohnnyAppleseed@gmail.com"
+            className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm bg-transparent"
+        />
+    </label>
+    )
+}
 
-//   plugins: [require('@tailwindcss/forms')]
-// -->
 
 // <label
 //   for="UserEmail"
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -24,7 +24,7 @@ export default function Nav() {
                                     fill="none"
                                     viewBox="0 0 24 24"
                                     stroke="currentColor"
-                                    stroke-width="2"
+                                    strokeWidth="2"
                                 >
                                 <path
                                     stroke-linecap="round"
