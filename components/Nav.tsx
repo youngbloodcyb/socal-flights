cad17d390c7d13ed6c85c5e6b4e4e9b082e4de46
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-21 18:42:17 -0700

    add small drop shadow below nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,27 +1,38 @@
 export default function Nav() {
     return (
-        <nav className="font-mono">
+        <nav className="font-mono shadow-sm shadow-white">
             <div className="w-screen h-20 flex flex-row items-center">
                 <div className="w-1/2" id="home-button-container">
-                    <h1 className="px-10">SoCal*Flights</h1>
+                    <h1 className="px-4">SoCal*Flights</h1>
                 </div>
                 <div id="link-container" className=" w-1/2 flex justify-end">
-                    <ul className="flex flex-row gap-4 px-4">
+                    <ul className="flex flex-row gap-4 px-2">
                         <li>
-                            <a href="/">See Flights</a>
+                            <a href="/">Flights</a>
                         </li>
                         <li>
                             <a href="/">Sign Up</a>
                         </li>
                         <li>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="white"
-                            stroke-width="2"
-                        />
+                            <a href="/">Login</a>
+                        </li>
+                        <li>
+                            <a href="/">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-5"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    stroke-width="2"
+                                >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                                />
+                                </svg>
+                            </a>
                         </li>
                     </ul>
                 </div>
