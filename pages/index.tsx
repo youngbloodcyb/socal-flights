3f1ba9054c144fca9f88cda2a95065ee24e5e6c2
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-21 18:25:17 -0700

    add nav and input components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
-    <h1 className='text-3xl text-red-500'>Hello World</h1>
+    <div>
+      <Nav/>
+      <h1 className='text-3xl text-red-500'>Hello World</h1>
+    </div>
   )
 }
